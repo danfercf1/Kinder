c7ce220fad54d1f606dd5cdfd039dbfc71eca6f2
Daniel Candia Flores <hunterofhell666@gmail.com>
2012-05-25 10:08:09 -0400

    PopUp registro y Login

--- views/registro.php
+++ views/registro.php
@@ -2,11 +2,13 @@
     <span id="titulo">Bienvenido</span>
     <form id="form_registro" action="<?php echo ROOT_PATH;?>frontend/create" method="POST">
         <ul id="lista_formulario">
-            <li><label for="NOMBRES_USUARIO" class="etiqueta_flecha">Nombre</label><input name="NOMBRES_USUARIO" id="NOMBRES_USUARIO" type="text" value="<?php echo $_POST['NOMBRES_USUARIO'];?>" class="form_login required"/></li>
-            <li><label for="APELLIDOS_USUARIO" class="etiqueta_flecha">Apellido</label><input name="APELLIDOS_USUARIO" id="APELLIDOS_USUARIO" type="text" value="<?php echo $_POST['APELLIDOS_USUARIO'];?>" class="form_login required"/></li>
-            <li><label for="EMAIL_USUARIO" class="etiqueta_flecha">Mi mail</label><input name="EMAIL_USUARIO" id="EMAIL_USUARIO" type="text" value="<?php echo $_POST['EMAIL_USUARIO'];?>" class="form_login required email"/></li>
-            <li><label for="EMAILP_USUARIO" class="etiqueta_flecha">Mail de pap&aacute;</label><input name="EMAILP_USUARIO" id="EMAILP_USUARIO" type="text" value="<?php echo $_POST['EMAILP_USUARIO'];?>" class="form_login required email"/></li>
-            <li><label for="PASS_USUARIO" class="etiqueta_flecha">Contrase&ntilde;a</label><input name="PASS_USUARIO" id="PASS_USUARIO" type="password" class="form_login required"/></li>
+            <li><label for="NOMBRES_USUARIO" class="etiqueta_flecha">mi nombre</label><input name="NOMBRES_USUARIO" id="NOMBRES_USUARIO" type="text" value="<?php echo $_POST['NOMBRES_USUARIO'];?>" class="form_login required"/></li>
+            <li><label for="APELLIDOS_USUARIO" class="etiqueta_flecha">mi apellido</label><input name="APELLIDOS_USUARIO" id="APELLIDOS_USUARIO" type="text" value="<?php echo $_POST['APELLIDOS_USUARIO'];?>" class="form_login required"/></li>
+            <li><label for="FECHANAC_USUARIO" class="etiqueta_flecha">fecha de nacimiento</label><input name="FECHANAC_USUARIO" id="FECHANAC_USUARIO" type="text" value="<?php echo $_POST['FECHANAC_USUARIO'];?>" class="form_login required date"/></li>
+            <li><label for="EMAIL_USUARIO" class="etiqueta_flecha">mi mail</label><input name="EMAIL_USUARIO" id="EMAIL_USUARIO" type="text" value="<?php echo $_POST['EMAIL_USUARIO'];?>" class="form_login required email"/></li>
+            <li><label for="EMAILP_USUARIO" class="etiqueta_flecha">mail de uno de mis padres</label><input name="EMAILP_USUARIO" id="EMAILP_USUARIO" type="text" value="<?php echo $_POST['EMAILP_USUARIO'];?>" class="form_login required email"/></li>
+            <li><label for="PASS_USUARIO" class="etiqueta_flecha">contrase&ntilde;a</label><input name="PASS_USUARIO" id="PASS_USUARIO" type="password" class="form_login required"/></li>
+            <li><label for="REPASS_USUARIO" class="etiqueta_flecha">confirmar contrase&ntilde;a</label><input name="REPASS_USUARIO" id="REPASS_USUARIO" type="password" class="form_login required"/></li>
         </ul>
         <button class="boton_registro"></button>
     </form>
